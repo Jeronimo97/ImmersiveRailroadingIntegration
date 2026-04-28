bcd6231493e11c2ec6cc719df3957f53b78e3cc7
Philipp <philippdal@gmail.com>
2026-04-28 10:56:21 +0200

    ref: update methods

--- src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/BlockEntityTrackTickable.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/BlockEntityTrackTickable.java
@@ -2,7 +2,6 @@ package cam72cam.immersiverailroading.thirdparty.trackapi;
 
 import cam72cam.mod.block.BlockEntityTickable;
 import cam72cam.mod.block.tile.TileEntity;
-import cam72cam.mod.block.tile.TileEntityTickable;
 import cam72cam.mod.resource.Identifier;
 
 public abstract class BlockEntityTrackTickable extends BlockEntityTickable implements ITrack {
